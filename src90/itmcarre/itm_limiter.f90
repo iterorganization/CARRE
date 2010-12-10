4cac53c58539a64c7458cb70e61a6a78f9b120f3
Hans-Joachim Klingshirn <hmk@ipp.mpg.de>
2010-12-10 09:30:11 +0000

    Make itm_limiter compile
    git-svn-id: http://solps-mdsplus.aug.ipp.mpg.de/repos/CARRE/branches/hmk@93 4d76dbe9-ff4f-0410-8810-ffbc1acdba1b

--- src90/itmcarre/itm_limiter.f90
+++ src90/itmcarre/itm_limiter.f90
@@ -15,23 +15,60 @@ module itm_limiter_mockup
 
   type type_limelement  !    
           character(len=132), dimension(:), pointer ::name => null()       ! /limelement/name - Name of this element. Should be a matrix of strings (nlims,max_nelements), but not supported by the UAL yet.
-          character(len=132), dimension(:), pointer ::id => null()       ! /limelement/id - ID of this element. Should be a matrix of strings (nlims,max_nelements), but not supported by the UAL yet.
-          real(R8),pointer  :: area(:,:) => null()     ! /limelement/area - Surface area of this element [m^2]; Matrix (nlims,max_nelements)
+          !character(len=132), dimension(:), pointer ::id => null()       ! /limelement/id - ID of this element. Should be a matrix of strings (nlims,max_nelements), but not supported by the UAL yet.
           type (type_limgeometry) :: limgeometry  ! /limelement/limgeometry - 
   endtype type_limelement
+
+  integer, parameter :: ITM_STRUCT_OPEN = 0
+  integer, parameter :: ITM_STRUCT_CLOSED = 1
 
 
 contains
 
-
-  !.. nstruc: number of structures
-  !.. npstru: number of points per structure
-  !.. xstruc,ystruc: coordinates of the structure points
-  !                  (point index, structure index)
   
-  subroutine itmlimReadFromStructureFile() !'filename)
-    
+  subroutine itmlimReadFromStructureFile(filename, limiter)
+    character(*), input(in) :: filename    
+    type(type_limelement), intent(inout) :: limiter
 
+    ! internal    
+    
+    integer, parameter :: nstrmx = 100 ! max. number of structures
+    integer, parameter :: npstmx = 200 ! max. number of points per structure
+
+    integer :: nstruc,npstru(nstrmx)
+    real*8 :: xstruc(npstmx,nstrmx),ystruc(npstmx,nstrmx)
+    character nomstr(nstrmx)*80
+
+    integer :: is
+
+    ! open & read structure file
+    OPEN(UNIT=8, FILE=name, STATUS='old')
+    nstruc=0
+    call listru(8,nstruc,npstru,nomstr,xstruc,ystruc,npstmx,nstrmx)
+    ! ... file is closed in listru
+
+    ! transfer to CPO structure
+    allocate( limiter % name( nstruc ) )
+    allocate( limiter % limgeometry % npoints( nstruc, 1 ) )
+    allocate( limiter % limgeometry % closed( nstruc, 1 ) )
+    allocate( limiter % limgeometry % rzcoordinate % r( nstruc, 1, maxval( npstru ) ) )
+    allocate( limiter % limgeometry % rzcoordinate % z( nstruc, 1, maxval( npstru ) ) )
+
+
+    limiter % limgeometry % closed = ITM_STRUCT_OPEN
+    do is = 1, nstruc
+
+            limiter % name( is ) = nomstr( is )
+            limiter % limgeometry % npoints( is, 1 ) = abs( npstru( is ) )
+            if ( npstru < 0 ) then             
+                    limiter % limgeometry % closed( is, 1 ) = ITM_STRUCT_CLOSED
+            end if
+            limiter % limgeometry % rzcoordinate % r( is, 1, 1 : abs(npstru( is )) ) &
+                 & = xstruc(:,is)
+            limiter % limgeometry % rzcoordinate % z( is, 1, 1 : abs(npstru( is )) ) &
+                 & = ystruc(:,is)
+
+    end do
     
   end subroutine itmlimReadFromStructureFile
 
