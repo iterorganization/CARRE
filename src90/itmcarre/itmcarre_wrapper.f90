d4d48ac49448cef8c460c99983ab28816c72a1f9
Hans-Joachim Klingshirn <hmk@ipp.mpg.de>
2011-08-09 13:13:03 +0000

    Make measure information optional when writing b2 grids
    git-svn-id: http://solps-mdsplus.aug.ipp.mpg.de/repos/CARRE/branches/hmk@160 4d76dbe9-ff4f-0410-8810-ffbc1acdba1b

--- src90/itmcarre/itmcarre_wrapper.f90
+++ src90/itmcarre/itmcarre_wrapper.f90
@@ -14,26 +14,31 @@ program itmcarre_wrapper
   type(type_edge), pointer :: edgecpo(:)
 
   integer :: idx
+  real(R8) :: time
+
+  integer, parameter :: CLOSEST_SAMPLE = 1 ! until provided by ISIP
 
   ! read input cpos from UAL
   call logmsg( LOGDEBUG, "itmcarre_wrapper: reading cpos" )
   ! equilibrium
-  call open_ual(idx, nmlFile="ual.namelist.equilibrium")
-  call euitm_get(idx,"equilibrium",equcpo)
+  call open_ual(idx, time=time, nmlFile="ual.namelist.equilibrium")
+  allocate(equcpo(1))
+  call euitm_get_slice(idx, "equilibrium", equcpo(1), time, CLOSEST_SAMPLE)
   call close_ual(idx)
   ! limiter
   call open_ual(idx, nmlFile="ual.namelist.limiter")
-  call euitm_get(idx,"limiter",limcpo)
+  call euitm_get(idx, "limiter", limcpo)
   call close_ual(idx)  
 
   ! call itmcarre
   call logmsg( LOGDEBUG, "itmcarre_wrapper: main computation" )
-  call itmcarre_main(equcpo, limcpo, edgecpo )
+  call itmcarre_main(equcpo, limcpo, edgecpo)
 
   ! write output cpos to UAL
   call logmsg( LOGDEBUG, "itmcarre_wrapper: writing cpos" )
-  call open_ual(idx, nmlFile="ual.namelist.edge", doCreate=.true.)
-  call euitm_put(idx,"edge",edgecpo)
+  call open_ual(idx, time=time, nmlFile="ual.namelist.edge", doCreate=.true.)  
+  edgecpo(1)%time = time
+  call euitm_put_slice(idx, "edge", edgecpo(1))
   call close_ual(idx)
 
 end program itmcarre_wrapper
