54f93d683ef3a7c2a1a10ef10fffda6de4c07b8e
Hans-Joachim Klingshirn <hmk@ipp.mpg.de>
2011-12-07 13:13:34 +0000

    Minor improvement
    git-svn-id: http://solps-mdsplus.aug.ipp.mpg.de/repos/CARRE/branches/hmk@226 4d76dbe9-ff4f-0410-8810-ffbc1acdba1b

--- src90/carre_shared/tradui_constants.F90
+++ src90/carre_shared/tradui_constants.F90
@@ -1,8 +1,14 @@
 module tradui_constants
-
+  
+  use carre_constants
   implicit none
 
+  private
 
-  integer, parameter :: nisomx=1
+  integer, public, parameter :: nisomx=1
+
+  ! currently, cell flags computed in Carre are just passed through in traduit
+  integer, public, parameter :: GRID_N_CELLFLAGS = CARREOUT_NCELLFLAGS
   
 end module tradui_constants
+
